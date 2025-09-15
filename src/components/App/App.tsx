83613debaf3433d0e866efb19df953231f492a3e
deviwax <156660735+deviwax@users.noreply.github.com>
2025-09-15 20:28:02 +0300

    update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import SearchBar from "./components/SearchBar/SearchBar";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from './components/MovieModal/MovieModal';
-import type { Movie } from './types/movie';
-import { fetchMovies } from "./services/movieService";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from '../MovieModal/MovieModal';
+import type { Movie } from '../../types/movie';
+import { fetchMovies } from "../../services/movieService";
 import { Toaster, toast } from "react-hot-toast";
 import styles from './App.module.css';
 
